7a1139ea40fe6e188940441ef0925f750a33e138
ccmongx <chi251200@gmail.com>
2024-03-05 22:13:32 +0700

    minhdz

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float textureSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Vector2 currentOffset = renderer.material.GetTextureOffset("_MainTex");
+        renderer.material.SetTextureOffset("_MainTex", currentOffset + new Vector2(textureSpeed, 0) * Time.deltaTime);
+    }
+}
